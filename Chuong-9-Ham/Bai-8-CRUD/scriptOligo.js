f4e6064a61472212e8429a11ea534d8a1b5f4061
Võ Minh Quân <quanvoct@gmail.com>
2022-04-18 10:25:42 +0700

    update-bai-tap

--- Chuong-9-Ham/Bai-8-CRUD/scriptOligo.js
+++ Chuong-9-Ham/Bai-8-CRUD/scriptOligo.js
@@ -1,32 +1,31 @@
 
-        function validateInputArray(input, list) {
-            /*---------------------------------
-            Hàm kiểm tra từng ký tự trong chuỗi nhập vào có trong danh sách cho sẵn không
-            -----------------------------------*/
-            input = input.toUpperCase(); //Viết hoa chuỗi input lên
-            list = list.toUpperCase(); //Viết hoa chuỗi list lên
-            let inputArr = input.split("");
-            let listArr = list.split(",");
-            console.log(inputArr + ' và '+ listArr);
-            let result;
-            let vitri = []; 
-            for (let i = 0; i < inputArr.length; i++) {
-                let check = false; 
-                for (let j = 0; j < listArr.length; j++) {
-                    if (inputArr[i] == listArr[j]) {
-                        check = true;
-                    }
-                }
-                if (check == false) {
-                    vitri.push(i);
-                }
+function validateInputArray(input, list) {
+    /*---------------------------------
+    Hàm kiểm tra từng ký tự trong chuỗi nhập vào có trong danh sách cho sẵn không
+    -----------------------------------*/
+    input = input.replace(/\s/g, '').toUpperCase(); //Viết hoa chuỗi input lên
+    list = list.toUpperCase(); //Viết hoa chuỗi list lên
+    let inputArr = input.split("");
+    let listArr = list.split(",");
+    let result;
+    let vitri = [];
+    for (let i = 0; i < inputArr.length; i++) {
+        let check = false;
+        for (let j = 0; j < listArr.length; j++) {
+            if (inputArr[i] == listArr[j]) {
+                check = true;
             }
-            if (vitri.length > 0) {
-                let text = 'Chuỗi không hợp lệ tại vị trí: ';
-                text += vitri.join(', ');
-                result = text;
-            } else {
-                result = '';
-            }
-            return result;
         }
+        if (check == false) {
+            vitri.push(inputArr[i]);
+        }
+    }
+    if (vitri.length > 0) {
+        let text = 'Chuỗi không hợp lệ tại vị trí: ';
+        text += vitri.join(', ');
+        result = text;
+    } else {
+        result = '';
+    }
+    return result;
+}
